8046e626b81784eb6b23b2a5fe0c0c604af154cb
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-10-04 11:06:05 -0300

    Project created

---
+++ src/test/java/com/tidii/optimusapi/OptimusapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidii.optimusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimusapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
